e2aeee54e77fbfdd02fe8a58ce0b0b31c91de156
Casey Geist <deepimpact5@yahoo.com>
2021-07-19 12:11:59 -0400

    empty files

---
+++ jobs.js
@@ -0,0 +1,13 @@
+let USER_IP_ADDR;
+
+fetch('https://api.ipify.org/?format=json')
+    .then( resp => resp.json() )
+    .then( data => {
+        console.log( data + '\n' + data.ip );
+        USER_IP_ADDR = data.ip;
+        console.log(USER_IP_ADDR);
+    } );
+
+
+let ipDiv = document.getElementById('show-ip');
+ipDiv.innerHTML = `<span>${USER_IP_ADDR}</span>`;
