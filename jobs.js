9e72ded743fa4a59c981bcacf4db3699737faa5d
Casey Geist <deepimpact5@yahoo.com>
2021-08-02 09:16:53 -0400

    attempting to use .env file

--- jobs.js
+++ jobs.js
@@ -1,7 +1,16 @@
-const SEARCH_URL = 'https://data.usajobs.gov/api/search?Keyword=Software%20Engineer&LocationName=Boston%20Massachusetts&Radius=75&SortField=opendate&ResultsPerPage=20&Page=1';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const SEARCH_URL = 'https://data.usajobs.gov/api/search?Keyword=javascript&LocationName=Boston%20Massachusetts&Radius=75&SortField=opendate&ResultsPerPage=10&Page=1';
 const HOST = 'data.usajobs.gov';
-const MY_EMAIL = DERAIL;
-const API_KEY = SKI_SLOPE;
+const MY_EMAIL = process.env.MY_EMAIL;
+const API_KEY = process.env.API_KEY;
+
+let jobsData = {};                  // JSON object
+let numJobsReturned = 0;
+let newRowEntry = {};               // DOM object
+let numJobsDisplayed = 0;
+let numSavedJobs = 0;
 
 const jobsList = document.getElementById("jobs-list");          // Table body
 const entryCount = document.getElementById("entry-count");      // Span for # of jobs displayed
@@ -12,16 +21,9 @@ const updateForm = document.getElementById("form-update");
 let chosenDate;
 let dateChanged = false;
 
-let jobsData = {};
-let numJobsReturned = 0;
-let numJobsDisplayed = 0;
-let newRowEntry;
-let numSavedJobs = 0;
-
-
-// Use API with a search URL to fetch jobs data
+// Fetch jobs data via the API using a URL
 fetch(SEARCH_URL, {
-    method: 'GET',
+    method: 'GET',          // 'GET' is the default so this line is not required
     headers: {
         "Host": HOST,
         "User-Agent": MY_EMAIL,
@@ -48,10 +50,7 @@ function main() {
             displayJobs(10, numJobsReturned);
             // update entry count
             entryCount.innerText = `all ${numJobsReturned} posted`;
-            showAllBtn.setAttribute('value', 'disabled');
-            showAllBtn.disabled = true;
-            showAllBtn.classList.remove('text-black');
-            showAllBtn.classList.add('text-gray-400');
+            disableShowAll();
         }
     });
 
@@ -68,18 +67,16 @@ function main() {
             // grab chosen date as a string
             chosenDate = String(dateInput.value);                                   // "YYYY-MM-DD"
 
-            for(let i = numJobsDisplayed - 1; i > -1; i--) {
-                const thisJobsDate = document.getElementById(`date${i}`).innerText;
-                if(thisJobsDate < chosenDate) {                                     // @TODO: reverse logic
-                    const row = document.getElementById(`row${i}`);
-                    row.innerHTML = "";
-                    row.classList.add('hidden');
-                    numJobsDisplayed--;
+            for(let j = numJobsDisplayed - 1; j > -1; j--) {
+                const thisJobsDate = document.getElementById(`date${j}`).innerText;
+                if(thisJobsDate > chosenDate) {    // if chosen date is behind the oldest job shown then stop filtering
+                    break;
                 }
-                else break;
+                removeRow(j);
             }
             entryCount.innerText = `the ${numJobsDisplayed} most recent`;
             dateChanged = false;
+            disableShowAll();
         }
     });
 }
@@ -95,31 +92,41 @@ function displayJobs(from, howMany) {
     // take the lesser of:  the total # of jobs returned ...and... ith job we wish to see
     const upto = numJobsReturned < (from + howMany) ? numJobsReturned : (from + howMany) ;
 
-    for (let i = from; i < upto; i++) {
-        const thisJob = jobsData[i]["MatchedObjectDescriptor"];
-        const jobURL = thisJob["ApplyURI"][0];
-        const jobTitle = thisJob["PositionTitle"];
-        const companyName = thisJob["OrganizationName"];
-        const jobLocation = thisJob["PositionLocationDisplay"];
-        let payRate = String(thisJob["PositionRemuneration"][0]["MinimumRange"]);
-        payRate = payRate.substr(0, payRate.length - 2);
-        const jobType = thisJob["PositionSchedule"][0]["Name"];
+    for (let k = from; k < upto; k++) {
+        createRow(k);
+        numJobsDisplayed++;
+    }
+    // update table row entry count
+    entryCount.innerText = `the ${numJobsDisplayed} most recent`;
+}
 
-        const jobPostDate = String( thisJob["PublicationStartDate"] );           // "YYYY-MM-DD"
-        const postDateFormatted = String(
-                new Date( jobPostDate.replace('-', '/')     // "YYYY/MM/DD" to avoid 1 day off
-            )
-        ).substr(4, 11);                                             // Omit day of week
 
-        const closingDate = String( thisJob["ApplicationCloseDate"] );
-        const closeDateFormatted = String(
-            new Date( closingDate.replace('-', '/')
-            )
-        ).substr(4, 11);
+function createRow( i ) {
+    const thisJob = jobsData[i]["MatchedObjectDescriptor"];
+    const jobID = thisJob["MatchedObjectId"];                                // (number as a string)
+    const jobURL = thisJob["ApplyURI"][0];
+    const jobTitle = thisJob["PositionTitle"];
+    const companyName = thisJob["OrganizationName"];
+    const jobLocation = thisJob["PositionLocationDisplay"];
+    let payRate = String(thisJob["PositionRemuneration"][0]["MinimumRange"]);   // (number as a string)
+    payRate = payRate.substr(0, payRate.length - 2);                // remove ".0"
+    const jobType = thisJob["PositionSchedule"][0]["Name"];
 
-        newRowEntry = document.createElement('tr');
-        newRowEntry.id = `row${i}`;
-        newRowEntry.innerHTML = `<td><a href="${jobURL}" class="text-blue-700 underline">${jobTitle}</a></td> 
+    const jobPostDate = String( thisJob["PublicationStartDate"] );           // "YYYY-MM-DD"
+    const postDateFormatted = String(
+        new Date( jobPostDate.replace('-', '/')     // "YYYY/MM/DD" to avoid 1 day off
+        )
+    ).substr(4, 11);                                             // Omit day of week
+
+    const closingDate = String( thisJob["ApplicationCloseDate"] );
+    const closeDateFormatted = String(
+        new Date( closingDate.replace('-', '/')
+        )
+    ).substr(4, 11);
+
+    newRowEntry = document.createElement('tr');
+    newRowEntry.id = `${jobID}`;
+    newRowEntry.innerHTML = `<td><a href="${jobURL}" class="text-blue-700 underline">${jobTitle}</a></td> 
                 <td>${companyName}</td>
                 <td>${jobLocation}</td>
                 <td>$${payRate}</td>
@@ -127,38 +134,50 @@ function displayJobs(from, howMany) {
                 <td>${postDateFormatted}</td>
                 <td>${closeDateFormatted}</td>
                 <td id="star${i}"><i class="material-icons">star_border</i></td>
+                <td id="share${i}"><i class="material-icons">send</i></td>
                 <td id="date${i}" class="hidden">${jobPostDate}</td>`;
-        jobsList.appendChild(newRowEntry);
+    jobsList.appendChild(newRowEntry);
 
-        const temp = document.getElementById(`star${i}`);
-        temp.addEventListener('click', () => {
-            if(temp.innerHTML === "<i class=\"material-icons\">star_border</i>") {
-                temp.innerHTML = "<i class=\"material-icons text-yellow-500\">star</i>";
-                saveJob( document.getElementById(`row${i}`), i );
-            }
-            else {
-                temp.innerHTML = "<i class=\"material-icons\">star_border</i>";
-            }
-
-        });
-        numJobsDisplayed++;
-    }
-    // update table row entry count
-    entryCount.innerText = `the ${numJobsDisplayed} most recent`;
+    const temp = document.getElementById(`star${i}`);
+    temp.addEventListener('click', () => {
+        if(temp.innerHTML === "<i class=\"material-icons\">star_border</i>") {
+            temp.innerHTML = "<i class=\"material-icons text-yellow-500\">star</i>";
+            saveJob( jobID, newRowEntry );
+        }
+        else {
+            temp.innerHTML = "<i class=\"material-icons\">star_border</i>";
+        }
+    });
 }
 
-function saveJob( rowObj, rowID ) {
-    console.log("attempting to save a job ;-)");
-    // push that job onto the array/JSON and save that data & jobQty to localStorage
-    try {
-        localStorage.setItem(`${numSavedJobs}`, `${rowID}`);        // e.g. "0" : "5"
-        numSavedJobs++;
-        localStorage.setItem(`row${rowID}`, `${rowObj.innerHTML}`);   //  e.g. "row5" : "<td>JS Programmer</td>...<td> *star icon* </td>"
-        localStorage.setItem("savedJobsQty", `${numSavedJobs}`);
-    } catch(e) {
-        console.log("saving failed. sorry dude: " + e);
-    }
+function removeRow( i ) {
+    const row = document.getElementById(`row${i}`);
+    row.innerHTML = "";
+    row.classList.add('hidden');
+    numJobsDisplayed--;
+}
 
+function disableShowAll() {
+    // showAllBtn.setAttribute('value', 'disabled');
+    showAllBtn.disabled = true;
+    showAllBtn.classList.remove('text-black');
+    showAllBtn.classList.add('text-gray-400');
+}
+
+function saveJob( jobNumber, rowObj  ) {
+    // if that job doesn't already exist in local storage, then
+    if(localStorage.getItem(jobNumber) == null) {              // Does "" == null?  In case job is saved, deleted, and saved again.
+
+        // try to save that job data & update # of jobs saved           // @TODO: grab job row HTML by JOB ID # instead (make rowID the jobID)
+        try {
+            localStorage.setItem(`${numSavedJobs}`, jobNumber);        // e.g. "0"             : "609237100"
+            numSavedJobs++;
+            localStorage.setItem(jobNumber, `${rowObj.innerHTML}`);   //  e.g. "609237100"     : "<td>JS Programmer</td>...<td> *star icon* </td>"
+            localStorage.setItem("savedJobsQty", `${numSavedJobs}`);   // e.g. "savedJobsQty"  : "1"
+        } catch(e) {
+            console.log("saving failed. sorry dude: " + e);
+        }
+    }
 }
 
 
--- saved.js
+++ saved.js
@@ -1,17 +1,21 @@
 let numSavedJobs;
-let rowID;
-let jobData;
-const savedCount = document.getElementById("saved-count");
+let jobID;
+const savedText = document.getElementById("saved-text");
 const savedJobsList = document.getElementById("saved-jobs-list");
 
 
-// @TODO: use try/catch block
-numSavedJobs = localStorage.getItem("savedJobsQty") | 0;
+// @TODO: use try/catch block for reading from file
+numSavedJobs = Number( localStorage.getItem("savedJobsQty") ) | 0;
 
 for(let i = 0; i < numSavedJobs; i++) {
     let newTableRow = document.createElement("tr");
-    rowID = localStorage.getItem(`${i}`);                                   // get the original job row ID
-    newTableRow.innerHTML = localStorage.getItem(`row${rowID}`);            // get the HTML for that table row
+    jobID = localStorage.getItem(`${i}`);                                   // get the original job row ID
+    newTableRow.innerHTML = localStorage.getItem(jobID);            // get the HTML for that table row
     savedJobsList.appendChild(newTableRow);
 }
-if(numSavedJobs !== 0) savedCount.innerText = `Showing your ${numSavedJobs} saved job(s)`;
+if(numSavedJobs !== 0) savedText.innerText = `Showing your ${numSavedJobs} saved job(s)`;
+
+
+// localStorage.setItem(`${numSavedJobs}`, jobNumber);        // e.g. "0"             : "609237100"
+// numSavedJobs++;
+// localStorage.setItem(jobNumber, `${rowObj.innerHTML}`);   //  e.g. "609237100"     : "<td>JS Programmer</td>...<td> *star icon* </td>"
