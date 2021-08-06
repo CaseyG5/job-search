7a9a81532b7234985944556fd7fb2cc19fc0bb40
Casey Geist <deepimpact5@yahoo.com>
2021-08-06 17:48:28 -0400

    changed of variable name to mySavedJobs

--- saved.js
+++ saved.js
@@ -1,21 +1,16 @@
-let numSavedJobs;
+let mySavedJobs;
 let jobID;
 const savedText = document.getElementById("saved-text");
 const savedJobsList = document.getElementById("saved-jobs-list");
 
 
 // @TODO: use try/catch block for reading from file
-numSavedJobs = Number( localStorage.getItem("savedJobsQty") ) | 0;
+mySavedJobs = Number( localStorage.getItem("savedJobsQty") ) | 0;
 
-for(let i = 0; i < numSavedJobs; i++) {
+for(let i = 0; i < mySavedJobs; i++) {
     let newTableRow = document.createElement("tr");
-    jobID = localStorage.getItem(`${i}`);                                   // get the original job row ID
-    newTableRow.innerHTML = localStorage.getItem(jobID);            // get the HTML for that table row
+    jobID = localStorage.getItem(`${i}`);                       // get the job ID #
+    newTableRow.innerHTML = localStorage.getItem(jobID);            // get the table row HTML for that job
     savedJobsList.appendChild(newTableRow);
 }
-if(numSavedJobs !== 0) savedText.innerText = `Showing your ${numSavedJobs} saved job(s)`;
-
-
-// localStorage.setItem(`${numSavedJobs}`, jobNumber);        // e.g. "0"             : "609237100"
-// numSavedJobs++;
-// localStorage.setItem(jobNumber, `${rowObj.innerHTML}`);   //  e.g. "609237100"     : "<td>JS Programmer</td>...<td> *star icon* </td>"
+if(mySavedJobs !== 0) savedText.innerText = `Showing your ${mySavedJobs} saved job(s)`;
