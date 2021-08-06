eacfd8b3ce5854aa9742fa40287baeea2be3f313
Casey Geist <deepimpact5@yahoo.com>
2021-08-06 17:43:18 -0400

    put showall listener from jobs.js in own file

--- showall.js
+++ showall.js
@@ -1,15 +1,14 @@
 const showAllForm = document.getElementById("form-show-all");
-const showAllBtn = document.getElementById("show-all");
 
 let again = false;
 let fullTable = "";
 
 
-main();
+showAll();
 
 /////////////////////////////////////////////////////////////////////////
 
-function main() {
+function showAll() {
 
     showAllForm.addEventListener('submit', (event) => {
         event.preventDefault();                           // prevent submit button & form from jumping the gun
@@ -22,11 +21,7 @@ function main() {
             }
             else {
                 displayJobs(10, 10);            // 1 page of jobs at 20 jobs per page
-                // update entry count
-                //entryCount.innerText = `all ${numJobsReturned} posted`;
-                console.log("saving full table...");
                 fullTable = jobsList.innerHTML;
-                //disableShowAll();
                 enableFilters();
                 again = true;
             }
@@ -49,4 +44,5 @@ function restoreFullTable() {
     jobsList.innerHTML = fullTable;
     numJobsDisplayed = numJobsReturned;
     entryCount.innerText = `${numJobsDisplayed}`;
+    // @TODO: add back event listeners
 }
