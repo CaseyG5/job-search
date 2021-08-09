aa63dadb77c271857e0614d427d46727da819333
Casey Geist <deepimpact5@yahoo.com>
2021-08-09 15:50:14 -0400

    env variables

--- search.js
+++ search.js
@@ -1,46 +1,39 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const SEARCH_URL = 'https://data.usajobs.gov/api/search?Keyword=javascript&LocationName=Boston%20Massachusetts&Radius=75&SortField=opendate&ResultsPerPage=10&Page=1';
+import { MY_EMAIL, API_KEY } from 'babel-plugin-dotenv';
 const HOST = 'data.usajobs.gov';
-const MY_EMAIL = process.env.MY_EMAIL;
-const API_KEY = process.env.API_KEY;
-
+const myEmail = MY_EMAIL;
+const apiKey = API_KEY;
 const searchForm = document.getElementById('search-form');
-const searchBox = document.getElementById('search-box');
-//const searchBtn = document.getElementById('search-btn');
+const searchBox = document.getElementById('search-box'); //const searchBtn = document.getElementById('search-btn');
+
 let searchTerms = "";
-let NEW_SEARCH_URL
+let NEW_SEARCH_URL;
+let myJobsData = {}; // JSON object
+let myNumJobsReturned = 0;
 
-let myJobsData = {};                  // JSON object
-let numJobsReturned = 0;
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
 
-searchForm.addEventListener( 'submit', (event) => {
-    event.preventDefault();
+  if (searchBox.value) {
+    // ensure search field is not empty
+    searchTerms = searchBox.value;
+    searchTerms.replaceAll(' ', '%20'); // format spaces for search engine
 
-    if (searchBox.value) {           // ensure search field is not empty
-        searchTerms = searchBox.value;
-        searchTerms.replaceAll(' ', '%20');     // format spaces for search engine
-        searchBox.value = "";
-        NEW_SEARCH_URL = `https://data.usajobs.gov/api/search?Keyword=${searchTerms}&SortField=opendate&ResultsPerPage=10&Page=1`;
-
-        getJobs( NEW_SEARCH_URL );
-    }
+    searchBox.value = "";
+    NEW_SEARCH_URL = `https://data.usajobs.gov/api/search?Keyword=${searchTerms}&SortField=opendate&ResultsPerPage=10&Page=1`;
+    getJobs(NEW_SEARCH_URL);
+  }
 });
 
-const getJobs = async ( API_URL ) => {
-
-    await fetch(API_URL, {
-        headers: {
-            "Host": HOST,
-            "User-Agent": MY_EMAIL,
-            "Authorization-Key": API_KEY
-        }
-    }).then(resp => resp.json())
-        .then(json => {
-            myJobsData = formatJobsData( json["SearchResult"]["SearchResultItems"] );
-            numJobsReturned = myJobsData.length;
-            console.log( myJobsData );
-        });
-
+const getJobs = async API_URL => {
+  await fetch(API_URL, {
+    headers: {
+      "Host": HOST,
+      "User-Agent": myEmail,
+      "Authorization-Key": apiKey
+    }
+  }).then(resp => resp.json()).then(json => {
+    myJobsData = formatJobsData(json["SearchResult"]["SearchResultItems"]);
+    myNumJobsReturned = myJobsData.length;
+    console.log(myJobsData);
+  });
 };
